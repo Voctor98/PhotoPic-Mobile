daa0c2c81089b20b81005dfafe1f02f1cf70a9fc
ChrisRed20 <crisarman010@gmail.com>
2025-03-09 16:38:20 -0600

    Agregar formulario de información empresarial y servicio para gestionar empresas

--- src/app/pages/complete-info/complete-info.page.ts
+++ src/app/pages/complete-info/complete-info.page.ts
@@ -1,20 +1,34 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { FormsModule } from '@angular/forms';
-import { IonContent, IonHeader, IonTitle, IonToolbar } from '@ionic/angular/standalone';
+import { MatButtonModule } from "@angular/material/button";
+import { FormsModule, ReactiveFormsModule, FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { IonContent, IonHeader, IonTitle, IonToolbar, IonLabel, IonItem, IonButton, IonInput } from '@ionic/angular/standalone';
 
 @Component({
   selector: 'app-complete-info',
   templateUrl: './complete-info.page.html',
   styleUrls: ['./complete-info.page.scss'],
   standalone: true,
-  imports: [IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule]
+  imports: [IonInput, IonButton, IonItem, IonLabel, IonContent, IonHeader, IonToolbar, CommonModule, FormsModule, ReactiveFormsModule]
 })
-export class CompleteInfoPage implements OnInit {
+export class CompleteInfoPage {
+  businessForm: FormGroup;
 
-  constructor() { }
+  constructor(private fb: FormBuilder) {
+    this.businessForm = this.fb.group({
+      businessName: ['', [Validators.required, Validators.minLength(2)]],
+      businessType: ['', [Validators.required]],
+      businessAddress: ['', [Validators.required]],
+      businessPhone: ['', [Validators.required]],
+      businessEmail: ['', [Validators.required, Validators.email]],
+    });
+   }
 
-  ngOnInit() {
+  onSubmit() {
+    if (this.businessForm.valid) {
+      console.log('Form Data:', this.businessForm.value);
+    } else {
+      console.log('Form is invalid');
+    }
   }
-
 }
---
+++ src/app/services/enterprise.service.ts
@@ -0,0 +1,37 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class EnterpriseService {
+  private url = 'http://localhost:4000/api/enterprises';
+
+  constructor(private http: HttpClient) { }
+
+  // Create a new enterprise
+  createEnterprise(enterprise: any): Observable<any> {
+    return this.http.post<any>(`${this.url}`, enterprise);
+  }
+
+  // Read all enterprises
+  getEnterprises(): Observable<any[]> {
+    return this.http.get<any[]>(`${this.url}`);
+  }
+
+  // Read a single enterprise by ID
+  getEnterpriseById(id: string): Observable<any> {
+    return this.http.get<any>(`${this.url}/${id}`);
+  }
+
+  // Update an enterprise by ID
+  updateEnterprise(id: string, enterprise: any): Observable<any> {
+    return this.http.put<any>(`${this.url}/${id}`, enterprise);
+  }
+
+  // Delete an enterprise by ID
+  deleteEnterprise(id: string): Observable<any> {
+    return this.http.delete<any>(`${this.url}/${id}`);
+  }
+}
